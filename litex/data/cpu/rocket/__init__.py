1de2d1a12fce11ce1a074607aa3c5b765804df26
LiteX <robot@timvideos.us>
2020-04-06 15:28:01 +0000

    Updated using 0.0.post43 from https://github.com/litex-hub/litex-data-auto

--- litex/data/cpu/rocket/__init__.py
+++ litex/data/cpu/rocket/__init__.py
@@ -2,12 +2,35 @@ import os.path
 __dir__ = os.path.split(os.path.abspath(os.path.realpath(__file__)))[0]
 data_location = os.path.join(__dir__, "verilog")
 src = "https://github.com/chipsalliance/rocket-chip.git"
-git_hash = "1cf70ea2"
-git_describe = "v0.0-7004-g1cf70ea2"
-version_str = "0.0.post7004"
-version_tuple = (0, 0)
+
+# Module version
+version_str = "0.0.post7047"
+version_tuple = (0, 0, 7047)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post7004")
+    pversion = V("0.0.post7047")
+except ImportError:
+    pass
+
+# Data version info
+data_version_str = "0.0.post7004"
+data_version_tuple = (0, 0)
+try:
+    from packaging.version import Version as V
+    pdata_version = V("0.0.post7004")
+except ImportError:
+    pass
+data_git_hash = "1cf70ea2"
+data_git_describe = "v0.0-7004-g1cf70ea2"
+data_git_msg = """\
+
+"""
+
+# Tool version info
+tool_version_str = "0.0.post43"
+tool_version_tuple = (0, 0, 43)
+try:
+    from packaging.version import Version as V
+    ptool_version = V("0.0.post43")
 except ImportError:
     pass
--- test.py
+++ test.py
@@ -3,8 +3,18 @@ import os
 
 from litex.data.cpu import rocket
 
-print("Found rocket @ version", rocket.version_str, "("+rocket.git_hash+")")
+print("Found rocket @ version", rocket.version_str, "(with data", rocket.data_version_str, ")")
+print()
 print("Data is in", rocket.data_location)
 assert os.path.exists(rocket.data_location)
-print("It contains:\n -", end=" ")
-print("\n - ".join(os.listdir(rocket.data_location)))
+print("Data is version", rocket.data_version_str, rocket.data_git_hash)
+print("-"*75)
+print(rocket.data_git_msg)
+print("-"*75)
+print()
+print("It contains:")
+for root, dirs, files in os.walk(rocket.data_location):
+    dirs.sort()
+    for f in sorted(files):
+        path = os.path.relpath(os.path.join(root, f), rocket.data_location)
+        print(" -", path)
