8595dc02e5dbeb86035a92e97b1b93460d33ae43
LiteX Robot <robot@timvideos.us>
2021-02-04 03:37:49 +0000

    Updated using 0.0.post74 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_rocket/__init__.py
+++ pythondata_cpu_rocket/__init__.py
@@ -4,11 +4,11 @@ data_location = os.path.join(__dir__, "verilog")
 src = "https://github.com/chipsalliance/rocket-chip.git"
 
 # Module version
-version_str = "0.0.post7077"
-version_tuple = (0, 0, 7077)
+version_str = "0.0.post7078"
+version_tuple = (0, 0, 7078)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post7077")
+    pversion = V("0.0.post7078")
 except ImportError:
     pass
 
@@ -27,11 +27,11 @@ data_git_msg = """\
 """
 
 # Tool version info
-tool_version_str = "0.0.post73"
-tool_version_tuple = (0, 0, 73)
+tool_version_str = "0.0.post74"
+tool_version_tuple = (0, 0, 74)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post73")
+    ptool_version = V("0.0.post74")
 except ImportError:
     pass
 
