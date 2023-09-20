d97c0f6bf96790792b03f8f2fcff10a0d82b9d91
Gabriel Somlo <gsomlo@gmail.com>
2023-09-19 20:26:07 -0400

    Update to chipsalliance/rocket-chip commit #4f197707e

--- pythondata_cpu_rocket/verilog/generated-src/freechips.rocketchip.system.LitexConfig_full_1_1.behav_srams.v
+++ pythondata_cpu_rocket/verilog/generated-src/freechips.rocketchip.system.LitexConfig_full_1_1.behav_srams.v
@@ -1,4 +1,105 @@
 
+module cc_dir_ext(
+  input RW0_clk,
+  input [9:0] RW0_addr,
+  input RW0_en,
+  input RW0_wmode,
+  input [7:0] RW0_wmask,
+  input [151:0] RW0_wdata,
+  output [151:0] RW0_rdata
+);
+
+  reg reg_RW0_ren;
+  reg [9:0] reg_RW0_addr;
+  reg [151:0] ram [1023:0];
+  `ifdef RANDOMIZE_MEM_INIT
+    integer initvar;
+    initial begin
+      #`RANDOMIZE_DELAY begin end
+      for (initvar = 0; initvar < 1024; initvar = initvar+1)
+        ram[initvar] = {5 {$random}};
+      reg_RW0_addr = {1 {$random}};
+    end
+  `endif
+  integer i;
+  always @(posedge RW0_clk)
+    reg_RW0_ren <= RW0_en && !RW0_wmode;
+  always @(posedge RW0_clk)
+    if (RW0_en && !RW0_wmode) reg_RW0_addr <= RW0_addr;
+  always @(posedge RW0_clk)
+    if (RW0_en && RW0_wmode) begin
+      for(i=0;i<8;i=i+1) begin
+        if(RW0_wmask[i]) begin
+          ram[RW0_addr][i*19 +: 19] <= RW0_wdata[i*19 +: 19];
+        end
+      end
+    end
+  `ifdef RANDOMIZE_GARBAGE_ASSIGN
+  reg [159:0] RW0_random;
+  `ifdef RANDOMIZE_MEM_INIT
+    initial begin
+      #`RANDOMIZE_DELAY begin end
+      RW0_random = {$random, $random, $random, $random, $random};
+      reg_RW0_ren = RW0_random[0];
+    end
+  `endif
+  always @(posedge RW0_clk) RW0_random <= {$random, $random, $random, $random, $random};
+  assign RW0_rdata = reg_RW0_ren ? ram[reg_RW0_addr] : RW0_random[151:0];
+  `else
+  assign RW0_rdata = ram[reg_RW0_addr];
+  `endif
+
+endmodule
+
+module cc_banks_0_ext(
+  input RW0_clk,
+  input [13:0] RW0_addr,
+  input RW0_en,
+  input RW0_wmode,
+  input [63:0] RW0_wdata,
+  output [63:0] RW0_rdata
+);
+
+  reg reg_RW0_ren;
+  reg [13:0] reg_RW0_addr;
+  reg [63:0] ram [16383:0];
+  `ifdef RANDOMIZE_MEM_INIT
+    integer initvar;
+    initial begin
+      #`RANDOMIZE_DELAY begin end
+      for (initvar = 0; initvar < 16384; initvar = initvar+1)
+        ram[initvar] = {2 {$random}};
+      reg_RW0_addr = {1 {$random}};
+    end
+  `endif
+  integer i;
+  always @(posedge RW0_clk)
+    reg_RW0_ren <= RW0_en && !RW0_wmode;
+  always @(posedge RW0_clk)
+    if (RW0_en && !RW0_wmode) reg_RW0_addr <= RW0_addr;
+  always @(posedge RW0_clk)
+    if (RW0_en && RW0_wmode) begin
+      for(i=0;i<1;i=i+1) begin
+        ram[RW0_addr][i*64 +: 64] <= RW0_wdata[i*64 +: 64];
+      end
+    end
+  `ifdef RANDOMIZE_GARBAGE_ASSIGN
+  reg [63:0] RW0_random;
+  `ifdef RANDOMIZE_MEM_INIT
+    initial begin
+      #`RANDOMIZE_DELAY begin end
+      RW0_random = {$random, $random};
+      reg_RW0_ren = RW0_random[0];
+    end
+  `endif
+  always @(posedge RW0_clk) RW0_random <= {$random, $random};
+  assign RW0_rdata = reg_RW0_ren ? ram[reg_RW0_addr] : RW0_random[63:0];
+  `else
+  assign RW0_rdata = ram[reg_RW0_addr];
+  `endif
+
+endmodule
+
 module data_arrays_0_ext(
   input RW0_clk,
   input [8:0] RW0_addr,
